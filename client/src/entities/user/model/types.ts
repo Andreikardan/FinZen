0923660f06ad4fe27919392ea90009873b510a4d
FromSPb <www.14088@gmail.com>
2025-01-09 17:48:31 +0300

    я куда то жмав и оно заработало

--- client/src/entities/user/model/types.ts
+++ client/src/entities/user/model/types.ts
@@ -1,10 +1,12 @@
 export interface ISignInData {
+  [key:string]:string
   email: string;
   password: string;
 }
 
 export interface ISignUpData extends ISignInData {
   username: string;
+  repeat:string;
 }
 
 export type UserType = {
