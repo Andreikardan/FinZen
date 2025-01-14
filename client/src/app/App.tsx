0d8e89f54176cdd708adf7c2492bc31407aafea0
ArsenAntonyan522 <arstothen@gmail.com>
2025-01-14 12:10:02 +0300

    create LandingPage

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,50 +1,59 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-import { useMobileOrientation, isMobile } from 'react-device-detect'; 
-import { useEffect} from "react";
+import { isMobile, useMobileOrientation } from "react-device-detect";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useAppDispatch } from "@/shared/hooks/reduxHooks";
 import { refreshTokensThunk } from "@/entities/user";
+import { LandingPage } from "@/pages/LandingPage/LandingPage";
+import { Layout } from "antd";
 
 function App(): React.JSX.Element {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const [isDesktop, setIsDesktop] = useState(!isMobile);
   const { isLandscape } = useMobileOrientation();
 
-  const handleVisibilityChange =() => {
-    const state = document.visibilityState
+  const handleVisibilityChange = () => {
+    const state = document.visibilityState;
 
-    if (state === 'visible' && isMobile) {
-      dispatch(refreshTokensThunk()).unwrap()
+    if (state === "visible" && isMobile) {
+      dispatch(refreshTokensThunk()).unwrap();
     }
-  }
+  };
 
   useEffect(() => {
-    document.addEventListener('visibilitychange', handleVisibilityChange)
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 1000 && !isMobile);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
 
     return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange)
-}
-}, []);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
 
+  return (
+    <div className="container">
+      {isDesktop && <LandingPage />}
 
-return (
-   
-      <div className="container">
-        <RouterProvider router={router} />
-          {(isLandscape || window.innerWidth > 500) && 
-          <div className="orientation">
-            <div className="message">
-            <h2>Переверните телефон</h2>;
-            </div>
-          </div>}
-          {!isMobile && 
-          <div className="orientation">
-            <div className="message">
-            <h2>Такой формат не поддерживается</h2>;
-            </div>
-          </div>}
-      </div>
- 
+      {((isLandscape && isMobile) ||
+        (window.innerWidth > 500 && window.innerWidth < 1000)) && (
+        <div className="orientation">
+          <div className="message">
+            <h2>Переверните телефон</h2>
+          </div>
+        </div>
+      )}
+
+      {!isDesktop && isMobile && <Layout />}
+
+      {!isDesktop && <RouterProvider router={router} />}
+    </div>
   );
 }
 
--- client/src/shared/enums/routes.ts
+++ client/src/shared/enums/routes.ts
@@ -5,6 +5,7 @@ export enum ROUTES {
   OPERATIONS = "/operations",
   ANALYTICS = "/goals",
   GOAL_TRANSACTIONS = "/goal-transactions",
-  SETTINGS = '/settings'
+  SETTINGS = '/settings',
+  LANDING = '/welcome'
 
 }
