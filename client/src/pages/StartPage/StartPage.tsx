1ffa9adfe9dc718341a3d9a9edd2341fc7f15860
ArsenAntonyan522 <arstothen@gmail.com>
2025-01-15 16:56:03 +0300

    startPage style

--- client/src/pages/StartPage/StartPage.tsx
+++ client/src/pages/StartPage/StartPage.tsx
@@ -1,28 +1,19 @@
+import styles from './StartPage.module.css';
 import React, { useState } from 'react';
 import AuthForm from '@/features/auth/ui/AuthForm';
 
 export function StartPage(): React.ReactElement {
+  const [state, setState] = useState<boolean>(false);
 
-    const[state, setState] = useState<boolean>(false)
-
-    
   return (
-    <>
-        {!state && (
-            <button onClick={() => setState((prev) => !prev)}>
-            <span>ВХОД</span>
-            </button>
-        )}
-        {state && (
-          <>
-          <img className='img' src='/../../public/LOGO.png'   style={{
-    width: '300px',
-    height: '100px',
-    objectFit: 'cover'
-  }} />
-            <AuthForm/>
-            </>
-        )}
-    </>
+    <div className={styles.container}>
+      <img className={styles.logo} src='/../../public/LOGO.png' alt="Логотип" />
+      {!state && (
+        <button className={styles.enterButton} onClick={() => setState((prev) => !prev)}>
+          <span className={styles.enterButtonText}>ВХОД</span>
+        </button>
+      )}
+      {state && <AuthForm />}
+    </div>
   );
 }
