23a7c381524ba8ccdd006c21f60647deae698379
Andreikardan <timoshenko.andrei_1998@mail.ru>
2025-01-16 19:20:09 +0300

    addIcon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+
 const __dirname = path.resolve();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'FinZen - управление финансами',
+        short_name: 'FinZen',
+        description: 'Пример приложения с поддержкой PWA.',
+        theme_color: '#ffffff',
+        display: 'fullscreen',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{html,js,css,png,svg,jpg}'],
+        cleanupOutdatedCaches: true,
+      },
+    }),
+  ],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-})
+});
