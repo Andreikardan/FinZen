c9d8ecca4636249cfdc75e6b59bed27a494835af
FromSPb <www.14088@gmail.com>
2025-01-16 23:11:59 +0300

    final plz

--- client/src/widgets/UserEdit/UserEdit.tsx
+++ client/src/widgets/UserEdit/UserEdit.tsx
@@ -7,104 +7,148 @@ import { Button, Form, Input } from 'antd';
 import { Toast } from 'antd-mobile';
 import { useState } from 'react';
 
-export function UserEdit() {
-    const [pass, setPass] = useState<boolean>(false)
-    const dispatch = useAppDispatch()
-    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-    const user = useAppSelector((state)=>state.user.user)
+interface UserEditProps {
+  onSuccess: () => void;
+}
 
-    const submit = async (values: ISignUpData )=>{
-        for( const key in values){
-            if(values[key]===undefined){
-                delete values[key]
-            }
-        }
-        if (Object.keys(values).length===0){
-            return Toast.show({ content: "Все поля пустые", position: "bottom" });
-        }
-        if(values.email){
-            const isEmailExistsData = await isEmailExistsChecker(values.email);
-            if (!isEmailExistsData.data?.exists) {
-                Toast.show({ content: isEmailExistsData.message, position: "bottom" });
-                return;
-            }
-            values.email = values.email.toLowerCase()
-        }
-        const result = await dispatch(updateThunk(values));
-        unwrapResult(result);
-        Toast.show({
-            content: "Успешно",
-            position: "bottom",
-        });
+export function UserEdit({ onSuccess }: UserEditProps) {
+  const dispatch = useAppDispatch();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const user = useAppSelector((state) => state.user.user);
+
+  const submit = async (values: ISignUpData) => {
+    for (const key in values) {
+      if (values[key] === undefined || values[key] === '') {
+        delete values[key];
+      }
     }
 
+    if (Object.keys(values).length === 0) {
+      return Toast.show({ content: "Все поля пустые", position: "bottom" });
+    }
 
-    const handleFormChange = (allFields: any[]) => {
-        const hasErrors = allFields.some(({ errors }) => errors && errors.length > 0);
-        const passwordField = allFields.find((field) => field.name[0] === 'password');
-        const repeatField = allFields.find((field) => field.name[0] === 'repeat');
-        const password = passwordField?.value;
-        const repeat = repeatField?.value;
-        if (password && (!repeat || password !== repeat)) {
-            setIsButtonDisabled(true);
-            return;
-        }
-        setIsButtonDisabled(hasErrors || (password && password !== repeat));
-    };
+    if (values.email && values.email !== user!.email) {
+      const isEmailExistsData = await isEmailExistsChecker(values.email);
+      if (!isEmailExistsData.data?.exists) {
+        Toast.show({ content: isEmailExistsData.message, position: "bottom" });
+        return;
+      }
+      values.email = values.email.toLowerCase();
+    }
 
-    return (
-        <>
-            <Form style={{ maxWidth: '306px', minWidth: '306px' }} onFinish={submit} onFieldsChange={(_, changedFields) => handleFormChange(changedFields)} initialValues={{username: user!.username,email: user!.email}}>
-            <Form.Item name='email' hasFeedback rules={[{
-                validator: async (_,value) => {
-                    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                        if (value && !emailRegex.test(value)) {
-                            return Promise.reject('Введите корректный email');
-                        }
-                        return Promise.resolve();
-            }}]}>
-                <Input placeholder="email"/>
-            </Form.Item>
-            <Form.Item name="password" hasFeedback rules={[{ required: true, message: 'Пожалуйста, введите пароль' },{ 
-                validator: async (_, value) => {
-                    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    const result = await dispatch(updateThunk(values));
+    unwrapResult(result);
+    Toast.show({
+      content: "Успешно",
+      position: "bottom",
+    });
+
+    onSuccess();
+  };
+
+  const handleFormChange = (allFields: any[]) => {
+    const hasErrors = allFields.some(({ errors }) => errors && errors.length > 0);
+
+    const passwordField = allFields.find((field) => field.name[0] === 'password');
+    const repeatField = allFields.find((field) => field.name[0] === 'repeat');
+
+    const password = passwordField?.value;
+    const repeat = repeatField?.value;
+    if (password && (!repeat || password !== repeat)) {
+      setIsButtonDisabled(true);
+      return;
+    }
+    setIsButtonDisabled(hasErrors || (password && password !== repeat));
+  };
+
+  return (
+    <>
+      <Form
+        style={{ maxWidth: '306px', minWidth: '306px' }}
+        onFinish={submit}
+        onFieldsChange={(_, allFields) => handleFormChange(allFields)}
+        initialValues={{ username: user!.username, email: user!.email }}
+      >
+        <Form.Item
+          name='email'
+          hasFeedback
+          rules={[
+            {
+              validator: async (_, value) => {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (value && !emailRegex.test(value)) {
+                  return Promise.reject('Введите корректный email');
+                }
+                return Promise.resolve();
+              },
+            },
+          ]}
+        >
+          <Input placeholder="email" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          hasFeedback
+          rules={[
+            {
+              validator: async (_, value) => {
+                const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9])[a-zA-Z0-9!@#$%^&*]{8,}$/;
                 if (value && !passwordRegex.test(value)) {
-                    return Promise.reject('От 8 символов. Содержит спецсимвол, заглавную букву, цифры, и английские буквы');
+                  return Promise.reject('От 8 символов. Содержит спецсимвол, заглавную букву, цифры, и английские буквы');
                 }
                 return Promise.resolve();
-            }}]}>
-                <Input.Password placeholder="Введите пароль" />
-            </Form.Item>
-            <Form.Item name="repeat" hasFeedback dependencies={['password']} rules={[{ required: pass, message: 'Пожалуйста, повторите пароль' },{ required: true, message: 'Пожалуйста, введите повторно пароль' },
+              },
+            },
+          ]}
+        >
+          <Input.Password placeholder="Введите пароль" />
+        </Form.Item>
+
+        <Form.Item
+          name="repeat"
+          hasFeedback
+          dependencies={['password']}
+          rules={[
             ({ getFieldValue }) => ({
-                validator(_, value) {
-                if (!value) {
-                    setPass(()=>false)
-                    return Promise.resolve();
-                } else {
-                    setPass(()=>true)
+              validator(_, value) {
+                const password = getFieldValue('password');
+                if (password && !value) {
+                  return Promise.reject('Повторите пароль');
                 }
-                if (getFieldValue('password') === value) {
-                    return Promise.resolve();
-                }
-                return Promise.reject('Пароли не совпадают');
-            }})]}>
-            <Input.Password placeholder="Повторите пароль" />
-            </Form.Item>
-            <Form.Item name="username" hasFeedback rules={[{
-                validator: async (_, value) => {
-                    const usernameRegex = /^[a-zA-Z0-9]{4,}$/
-                if (value && !usernameRegex.test(value)) {
-                    return Promise.reject('Цифры, английские буквы, и длинна 4 символа');
+                if (password && value !== password) {
+                  return Promise.reject('Пароли не совпадают');
                 }
                 return Promise.resolve();
-            }}]}>
-                <Input placeholder="Введите имя пользователя" />
-            </Form.Item>
-            <Button type="primary" disabled={isButtonDisabled} htmlType="submit" style={{ width: '100%' }}>
-                Изменить
-            </Button>
-            </Form>
-        </>
-    );
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Повторите пароль" />
+        </Form.Item>
+
+        <Form.Item
+          name="username"
+          hasFeedback
+          rules={[
+            {
+              validator: async (_, value) => {
+                const usernameRegex = /^[a-zA-Z0-9]{4,}$/;
+                if (value && !usernameRegex.test(value)) {
+                  return Promise.reject('Цифры, английские буквы, и длина 4 символа');
+                }
+                return Promise.resolve();
+              },
+            },
+          ]}
+        >
+          <Input placeholder="Введите имя пользователя" />
+        </Form.Item>
+
+        <Button type="primary" disabled={isButtonDisabled} htmlType="submit" style={{ width: '100%' }}>
+          Изменить
+        </Button>
+      </Form>
+    </>
+  );
 }
